729bab1934d8108250a0977f6babefd5ce239d5b
arsumner96 <arsumner96@yahoo.com>
2023-11-09 15:31:40 -0800

    first commit

---
+++ FeedDog.py
@@ -0,0 +1,14 @@
+def feedDog(hunger_level, biscuit_size):
+    hunger_level.sort()
+    biscuit_size.sort()
+
+    dogs_fed = 0
+    for i in range(len(hunger_level)):
+        for biscuit in biscuit_size:
+            if biscuit_size[biscuit] >= hunger_level[i]:
+                dogs_fed += 1
+                biscuit_size.remove(biscuit_size[biscuit])
+                break
+    return dogs_fed
+
+# print(feedDog([2, 1],[1,3,2]))
